a9673046711b9085021c50451207a92ad261bb29
Alexander Bazo <alexanderbazo@googlemail.com>
2021-04-06 15:16:07 +0200

    Create TOC from assignment and make page usable on mobile devices

--- src/viewer/AssignmentViewer.js
+++ src/viewer/AssignmentViewer.js
@@ -22,6 +22,44 @@ function addLink(el, link, label) {
     linkEl.append(newLinkEl);
 }
 
+function updateTableOfContents(el) {
+    let title = el.querySelector(".content h1"),
+        headings = el.querySelectorAll(".content h2"),
+        tocEl = el.querySelector(".toc");
+    tocEl.append(createEntryToTableOfContents(title, "Start"));
+    for (let i = 0; i < headings.length; i++) {
+        tocEl.append(createEntryToTableOfContents(headings[i]));
+    }
+}
+
+function createEntryToTableOfContents(el, title) {
+    let newEl = document.createElement("li");
+    newEl.classList.add("entry");
+    newEl.innerHTML = title || el.innerHTML;
+    newEl.setAttribute("data-target-id", el.id);
+    newEl.addEventListener("click", onEntryInTableOfContentsClicked);
+    return newEl;
+}
+
+function onEntryInTableOfContentsClicked(event) {
+    let targetEl = document.querySelector(`#${event.target.getAttribute("data-target-id")}`);
+    targetEl.scrollIntoView({
+        behavior: "smooth",
+    });
+}
+
+function updateImageElements(el) {
+    let images = el.querySelectorAll(".content p img");
+    for (let i = 0; i < images.length; i++) {
+        images[i].parentElement.nextElementSibling.classList.add("image-label");
+    }
+}
+
+function enableLinkToTableOfContents() {
+    let tocLink = document.querySelector(".toc-link");
+    tocLink.addEventListener("click", () => document.querySelector(".toc-header").scrollIntoView());
+}
+
 class AssignmentViewer {
 
     constructor() {
@@ -31,6 +69,9 @@ class AssignmentViewer {
     render(assignment) {
         updateMetaInformation(this.el, assignment);
         updateContent(this.el, assignment);
+        updateTableOfContents(this.el);
+        updateImageElements(this.el);
+        enableLinkToTableOfContents();
         addLink(this.el, assignment.starter, "Startercode");
         addLink(this.el, assignment.solution, "Lösungsvorschlag");
     }
